80561c2db5ce1361a7a558ed89649d238cf0812d
Ethan Bykerk <e.bykerk@icloud.com>
2019-07-22 22:00:10 -0500

    commiting error on index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Watch this video</Link>
   </Layout>
 )
 
 export default IndexPage
+
